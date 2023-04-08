e8345fbf56620194880f906d42d9fae7718661eb
Sachin Kumar Sharma <sachinkumarsharma1234@gmail.com>
2023-04-08 17:49:48 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        System.out.println("the product is:" + (a * b));
+    };
+
+}
